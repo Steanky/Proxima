85ee35e3ef434b6d23d78a34659ac8499a7a1753
steank <dnd.initiative.tracker@gmail.com>
2022-10-22 01:19:52 -0700

    how is everything working correctly

--- core/src/test/java/com/github/steanky/proxima/WalkNodeSnapperTest.java
+++ core/src/test/java/com/github/steanky/proxima/WalkNodeSnapperTest.java
@@ -55,4 +55,73 @@ class WalkNodeSnapperTest {
 
         assertTrue(callIndicator.get(), "NodeHandler was not called");
     }
+
+    @Test
+    void higherJump() {
+        HashSpace space = new HashSpace(-10, -10, -10, 20, 20, 20);
+        space.put(0, 0, 0, Solid.FULL);
+        space.put(1, 2, 0, Solid.FULL);
+
+        Node node = new Node(0, 1, 0, 0, 0, null);
+        WalkNodeSnapper snapper = new WalkNodeSnapper(1, 2, 4, 2, space);
+
+        Wrapper<Boolean> callIndicator = Wrapper.of(false);
+        snapper.snap(Direction.EAST, node, (n, x, y, z) -> {
+            callIndicator.set(true);
+
+            assertSame(node, n);
+            assertEquals(1, x);
+            assertEquals(3, y);
+            assertEquals(0, z);
+        });
+
+        assertTrue(callIndicator.get(), "NodeHandler was not called");
+    }
+
+    @Test
+    void higherJumpFail() {
+        HashSpace space = new HashSpace(-10, -10, -10, 20, 20, 20);
+        space.put(0, 0, 0, Solid.FULL);
+        space.put(1, 2, 0, Solid.FULL);
+
+        Node node = new Node(0, 1, 0, 0, 0, null);
+        WalkNodeSnapper snapper = new WalkNodeSnapper(1, 2, 4, 1, space);
+
+        snapper.snap(Direction.EAST, node, (n, x, y, z) -> fail("NodeHandler should not be called"));
+    }
+
+    @Test
+    void jumpWithSizedHole() {
+        HashSpace space = new HashSpace(-10, -10, -10, 20, 20, 20);
+        space.put(0, 0, 0, Solid.FULL);
+        space.put(1, 1, 0, Solid.FULL);
+        space.put(1, 4, 0, Solid.FULL);
+
+        Node node = new Node(0, 1, 0, 0, 0, null);
+        WalkNodeSnapper snapper = new WalkNodeSnapper(1, 2, 4, 1, space);
+
+        Wrapper<Boolean> callIndicator = Wrapper.of(false);
+        snapper.snap(Direction.EAST, node, (n, x, y, z) -> {
+            callIndicator.set(true);
+
+            assertSame(node, n);
+            assertEquals(1, x);
+            assertEquals(2, y);
+            assertEquals(0, z);
+        });
+
+        assertTrue(callIndicator.get(), "NodeHandler was not called");
+    }
+
+    @Test
+    void jumpHitHead() {
+        HashSpace space = new HashSpace(-10, -10, -10, 20, 20, 20);
+        space.put(0, 0, 0, Solid.FULL);
+        space.put(1, 1, 0, Solid.FULL);
+        space.put(0, 3, 0, Solid.FULL);
+
+        Node node = new Node(0, 1, 0, 0, 0, null);
+        WalkNodeSnapper snapper = new WalkNodeSnapper(1, 2, 4, 1, space);
+        snapper.snap(Direction.EAST, node, (n, x, y, z) -> fail("NodeHandler should not be called"));
+    }
 }
