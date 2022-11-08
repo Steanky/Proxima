2f962ab04dff60993dea3e22b2068e726a5afaba
steank <dnd.initiative.tracker@gmail.com>
2022-11-07 21:06:28 -0800

    partial, more complex algorithm for WalkNodeSnapper

--- core/src/main/java/com/github/steanky/proxima/WalkNodeSnapper.java
+++ core/src/main/java/com/github/steanky/proxima/WalkNodeSnapper.java
@@ -9,186 +9,169 @@ import org.jetbrains.annotations.NotNull;
 import java.util.Objects;
 
 public class WalkNodeSnapper implements DirectionalNodeSnapper {
-    private final int height;
+    private static final double EPSILON = 1E-6;
+
+    private final double width;
+    private final double halfWidth;
+    private final double height;
+
+    private final boolean full;
+
+    private final int blockHeight;
+    private final int halfBlockWidth;
 
     private final Vec3I[] hDeltas;
     private final Vec3I[] vDeltas;
 
-    private final int fallTolerance;
-    private final int jumpHeight;
+    private final double fallTolerance;
+    private final double jumpHeight;
     private final Space space;
 
     private final Bounds3I searchArea;
 
-    public WalkNodeSnapper(int width, int height, int fallTolerance, int jumpHeight, @NotNull Space space,
+    public WalkNodeSnapper(double width, double height, double fallTolerance, double jumpHeight, @NotNull Space space,
             @NotNull Bounds3I searchArea) {
-        if ((width & 1) == 0) {
-            throw new IllegalArgumentException("Width must be an odd number");
+        if (width < 0 || height < 0 || fallTolerance < 0 || jumpHeight < 0 || !Double.isFinite(width) ||
+                !Double.isFinite(height)) {
+            throw new IllegalArgumentException("Invalid parameter");
         }
 
+        this.width = width;
+        this.halfWidth = width / 2;
         this.height = height;
-        int halfWidth = width >> 1;
 
-        this.hDeltas = new Vec3I[width * height];
-        this.vDeltas = new Vec3I[width * width];
+        this.full = width == Math.rint(width);
 
-        for (int h = 0; h < height; h++) {
-            for (int w = 0; w < width; w++) {
+        int blockWidth = ((int)Math.ceil(width)) | 1;
+        this.blockHeight = (int)Math.ceil(height);
+
+        this.halfBlockWidth = blockWidth >> 1;
+
+        this.hDeltas = new Vec3I[blockWidth * blockHeight];
+        this.vDeltas = new Vec3I[blockWidth * blockWidth];
+
+        for (int h = 0; h < blockHeight; h++) {
+            for (int w = 0; w < blockWidth; w++) {
                 //this is effectively a two-dimensional vector, z is always 0
                 //how this is interpreted depends on the direction
                 //these vectors are small enough that, unless we have really large entities, they will be cached
-                hDeltas[h * width + w] = Vec3I.immutable(w - halfWidth, h, 0);
+                hDeltas[h * blockHeight + w] = Vec3I.immutable(w - halfBlockWidth, h, 0);
             }
         }
 
-        for (int x = 0; x < width; x++) {
-            for (int z = 0; z < width; z++) {
+        for (int x = 0; x < blockWidth; x++) {
+            for (int z = 0; z < blockWidth; z++) {
                 //vertical deltas are simple
-                vDeltas[x * width + z] = Vec3I.immutable(x - halfWidth, 0, z - halfWidth);
+                vDeltas[x * blockWidth + z] = Vec3I.immutable(x - halfBlockWidth, 0, z - halfBlockWidth);
             }
         }
 
         this.fallTolerance = fallTolerance;
-        this.jumpHeight = jumpHeight;
+        this.jumpHeight = jumpHeight + EPSILON;
         this.space = Objects.requireNonNull(space);
         this.searchArea = Objects.requireNonNull(searchArea);
     }
 
     @Override
     public void snap(@NotNull Direction direction, @NotNull Node node, @NotNull NodeHandler handler) {
-        int nX = node.x + direction.x;
-        int nY = node.y + direction.y;
-        int nZ = node.z + direction.z;
+        int dx = direction.x;
+        int dy = direction.y;
+        int dz = direction.z;
 
-        if (!searchArea.contains(nX, nY, nZ)) {
+        int nx = node.x + dx;
+        int ny = node.y + dy;
+        int nz = node.z + dz;
+
+        if (!searchArea.contains(nx, ny, nz)) {
+            //fast exist: don't check out of bounds
             return;
         }
 
-        if (direction.y == 0) {
-            boolean tryJump = false;
-            int highestY = -1; //only set if tryJump is true
+        int hx = 0;
+        int hy = 0;
+        int hz = 0;
 
-            //first check: blocks we can run into horizontally
-            for (Vec3I delta : hDeltas) {
-                int x = nX + (direction.x == 0 ? delta.x() : 0);
-                int y = node.y + delta.y();
-                int z = nZ + (direction.z == 0 ? delta.x() : 0);
+        double highestY = Double.NEGATIVE_INFINITY;
+        Solid highestSolid = null;
+
+        //first, check the blocks in front of us
+        for (Vec3I delta : hDeltas) {
+            int dh = delta.x(); //translation along the axis perpendicular to the direction
+            int dv = delta.y(); //translation above the agent
+
+            if (!full) {
+                //first block we may encounter
+                int x = dx == 0 ? nx + dh : node.x + (dx * halfBlockWidth);
+                int y = ny + dv;
+                int z = dz == 0 ? nz + dh : node.z + (dz * halfBlockWidth);
 
                 Solid solid = space.solidAt(x, y, z);
-                if (!solid.isEmpty()) {
+
+                //if the solid is partial, we might not already be overlapping it
+                //if the solid is full, we know we're overlapping it
+                //if the solid is empty, it has no collision
+                if (!solid.isEmpty() && !solid.isFull()) {
                     Bounds3D bounds = solid.bounds();
-                    if (delta.y() + bounds.lengthY() > jumpHeight) {
-                        //fast exit: we can't go this way, there's a block we can't jump over
-                        return;
-                    }
 
-                    highestY = y + 1; //hDeltas increases in height
-                    tryJump = true;
-                }
-            }
+                    double box = x + bounds.originX();
+                    double boz = z + bounds.originZ();
 
-            if (tryJump) {
-                //first, check blocks above us
-                //also limit jump height according to blocks we might run into
-                int highestPossibleJump = jumpHeight;
+                    double aox = node.x + 0.5D - halfWidth;
+                    double aoz = node.z + 0.5D - halfWidth;
 
-                outer:
-                for (int i = 0; i < jumpHeight; i++) {
-                    int y = node.y + height + i;
+                    if (!overlaps(box, boz, bounds.lengthX(), bounds.lengthZ(), aox, aoz, width, width)) {
+                        double height = y + bounds.lengthY() - node.y;
 
-                    for (Vec3I delta : vDeltas) {
-                        int x = node.x + delta.x();
-                        int z = node.z + delta.z();
-
-                        Solid solid = space.solidAt(x, y, z);
-                        if (!solid.isEmpty()) {
-                            if (i == 0) {
-                                //fast exit: we hit our head on a block directly above us
-                                return;
-                            }
-
-                            if (highestY > y - height) {
-                                //fast exit: the highest block we initially collided with is above where we just hit
-                                return;
-                            }
-
-                            highestPossibleJump = i;
-                            break outer;
-                        }
-                    }
-                }
-
-                //then, check blocks below
-                int lastSolid = highestY;
-                for (int i = highestY - node.y; i <= highestPossibleJump; i += height) {
-                    boolean foundSolid = false;
-                    for (Vec3I delta : hDeltas) {
-                        int x = nX + (direction.x == 0 ? delta.x() : 0);
-                        int y = node.y + i + delta.y() + 1;
-                        int z = nZ + (direction.z == 0 ? delta.x() : 0);
-
-                        Solid solid = space.solidAt(x, y, z);
-                        if (!solid.isEmpty()) {
-                            foundSolid = true;
-
-                            if (y - lastSolid >= height) { //found a gap we can fit in
-                                //this node is bidirectional, unless the jump distance is greater than the fall
-                                //tolerance
-                                handleIfInBounds(handler, node, nX, lastSolid, nZ);
-                                return;
-                            }
-
-                            lastSolid = y + 1;
-                            break;
-                        }
-                        else if ((y + 1) - lastSolid >= height) {
-                            handleIfInBounds(handler, node, nX, lastSolid, nZ);
+                        if (height > jumpHeight) {
+                            //fast exit: can't jump over this block
                             return;
                         }
+
+                        //track the highest solid this layer
+                        if (height > highestY) {
+                            highestY = height;
+                            highestSolid = solid;
+
+                            hx = x;
+                            hy = y;
+                            hz = z;
+                        }
                     }
-
-                    if (!foundSolid) {
-                        handleIfInBounds(handler, node, nX, lastSolid, nZ);
-                        return;
-                    }
-                }
-
-                return;
-            }
-        }
-        else if(direction.y == 1) {
-            if (jumpHeight == 0) {
-                return;
-            }
-
-            for (Vec3I delta : vDeltas) {
-                int x = node.x + delta.x();
-                int y = node.y + height;
-                int z = node.z + delta.z();
-
-                Solid solid = space.solidAt(x, y, z);
-                if (!solid.isEmpty()) {
-                    return;
                 }
             }
 
-            handleIfInBounds(handler, node, node.x, node.y + 1, node.z);
-            return;
-        }
+            //second block we might encounter (first block, if full)
+            int x = dx == 0 ? nx + dh : nx + (dx * halfBlockWidth);
+            int y = ny + dv;
+            int z = dz == 0 ? nz + dh : nz + (dz * halfBlockWidth);
 
-        for (int i = 1; i <= fallTolerance; i++) { //check below us a maximum of fallTolerance blocks
-            for (Vec3I delta : vDeltas) {
-                int x = nX + delta.x();
-                int y = node.y - i;
-                int z = nZ + delta.z();
+            Solid solid = space.solidAt(x, y, z);
+            if (solid.isEmpty()) {
+                //nothing to do this iteration if empty
+                continue;
+            }
 
-                Solid solid = space.solidAt(x, y, z);
-                if (!solid.isEmpty()) { //we hit a solid on the way down
-                    handleIfInBounds(handler, node, nX, y + 1, nZ);
-                    return;
+            if (!solid.isFull()) {
+                //more detailed check necessary, we might not collide with this solid
+                Bounds3D bounds = solid.bounds();
+
+                double box = x + bounds.originX();
+                double boz = z + bounds.originZ();
+
+                double aox = nx + 0.5D - halfWidth;
+                double aoz = nz + 0.5D - halfWidth;
+
+                if (overlaps(box, boz, bounds.lengthX(), bounds.lengthZ(), aox, aoz, width, width)) {
+
                 }
             }
         }
+    }
+
+    //checks for overlap (in two dimensions); applies an epsilon value
+    private static boolean overlaps(double ox1, double oz1, double lx1, double lz1,
+            double ox2, double oz2, double lx2, double lz2) {
+        return false;
     }
 
     @Override
@@ -197,11 +180,5 @@ public class WalkNodeSnapper implements DirectionalNodeSnapper {
         int dZ = parent.z - currentNode.z;
 
         return Math.signum(dX) == direction.x && Math.signum(dZ) == direction.z;
-    }
-
-    private void handleIfInBounds(NodeHandler handler, Node node, int x, int y, int z) {
-        if (searchArea.contains(x, y, z)) {
-            handler.handle(node, x, y, z);
-        }
     }
 }
