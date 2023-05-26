4feda0cf737ecbc397d01c02537b291fdd18ab8b
steank <dnd.initiative.tracker@gmail.com>
2023-05-26 05:27:41 -0700

    fix edge case where DiagonalProcessor would prune a node near the end, leading to an invalid path

--- core/src/main/java/com/github/steanky/proxima/node/DiagonalProcessor.java
+++ core/src/main/java/com/github/steanky/proxima/node/DiagonalProcessor.java
@@ -27,7 +27,7 @@ class DiagonalProcessor implements NodeProcessor {
             }
 
             Node next = current.parent;
-            if (next == null) {
+            if (next == null || next.parent == null) {
                 break;
             }
 
